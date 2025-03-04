53735918cb8ec3c7ece28c98473bf06261ded362
diko <diko93@yandex.ru>
2025-03-04 14:19:28 +0500

    fix: minor fixes for urls

--- dao/urls.py
+++ dao/urls.py
@@ -6,8 +6,8 @@ app_name = "dao"
 
 urlpatterns = [
     path("", ActiveDaosView.as_view({"get": "list"}), name="daos-list"),
-    path("-fetch/", DaoInitialView.as_view({"post": "create"}), name="dao-fetch"),
-    path("-save/", DaoCompleteView.as_view({"patch": "update"}), name="dao-save"),
+    path("fetch/", DaoInitialView.as_view({"post": "create"}), name="dao-fetch"),
+    path("save/", DaoCompleteView.as_view({"patch": "update"}), name="dao-save"),
     path(
         "<slug:slug>/info/",
         ActiveDaosView.as_view({"get": "retrieve"}),
